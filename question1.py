6a61751c1dcefa811954f92b4c8d7fd75cf564a0
oxlngo <96035405+oxlngo@users.noreply.github.com>
2022-03-27 23:11:01 -0400

    modify csv

--- question1.py
+++ question1.py
@@ -5,7 +5,7 @@ from process import Process
 from process import csv_to_processes
 
 #returns an array of Process objects
-array_of_processes = csv_to_processes('data/processes.csv')
+array_of_processes = csv_to_processes('data/processes_1.csv')
 # print(array_of_processes)
 
 print(RR(array_of_processes))
